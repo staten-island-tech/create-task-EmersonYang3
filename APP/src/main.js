2c58c1154ce9f46c65bade22499458a0d73b32d0
EmersonYang3 <emersonyang28@gmail.com>
2024-12-21 12:56:51 -0500

    Coal Generation Test

--- APP/src/main.js
+++ APP/src/main.js
@@ -2,8 +2,8 @@ const configs = {
   minimumBet: 0.0,
   defaultCoal: 3,
   defaultBalance: 1000.0,
-  safePresentImage: "images/",
-  coalImage: "images/",
+  safePresentImage: "images/coal_pile.png",
+  coalImage: "src/images/coal_pile.png",
 };
 
 const DOMSelectors = {
@@ -14,18 +14,59 @@ const DOMSelectors = {
   winField: document.getElementById("total-payout"),
   betButton: document.querySelector(".bet-button"),
   presentContainer: document.querySelector(".present-container"),
+  balance: document.getElementById("balance"),
 };
 
 let gameStatus = {
+  gameInSession: false,
   betAmount: configs.minimumBet,
   coalAmount: configs.defaultCoal,
   balance: configs.defaultBalance,
+  safePresentsLeft: 0,
+  coalPresentsLeft: 0,
   board: [],
 };
 
 function applyConfigs() {
+  DOMSelectors.balance.textContent =
+    "Balance: $" + configs.defaultBalance.toFixed(2);
   DOMSelectors.numberOfCoalField.value = configs.defaultCoal;
 }
+
+const gameBoardService = {
+  populateCoals() {
+    gameStatus.board = [];
+    gameStatus.coalPresentsLeft = 0;
+    gameStatus.safePresentsLeft = 0;
+
+    for (let i = 0; i < gameStatus.coalAmount; i++) {
+      gameStatus.board.push(1);
+      gameStatus.coalPresentsLeft++;
+    }
+
+    while (gameStatus.board.length < 25) {
+      gameStatus.board.push(0);
+      gameStatus.safePresentsLeft++;
+    }
+
+    for (let i = gameStatus.board.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [gameStatus.board[i], gameStatus.board[j]] = [
+        gameStatus.board[j],
+        gameStatus.board[i],
+      ];
+    }
+
+    for (let i = 0; i < gameStatus.board.length; i++) {
+      const button = document.getElementById((i + 1).toString());
+      if (gameStatus.board[i] === 1) {
+        button.style.backgroundImage = `url(${configs.coalImage})`;
+      }
+    }
+
+    console.log(gameStatus.board);
+  },
+};
 
 const betSettingService = {
   betInput(input) {
@@ -53,17 +94,29 @@ const betSettingService = {
   },
 
   betStart() {
-    [
-      "betAmountField",
-      "numberOfCoalField",
-      "betButton",
-      "halfBetButton",
-      "doubleBetButton",
-    ].forEach((id) => {
-      DOMSelectors[id].disabled = true;
-    });
+    if (!gameStatus.betAmount) return;
 
-    DOMSelectors.betButton.textContent = "Leave The Factory...";
+    if (!gameStatus.gameInSession) {
+      [
+        "betAmountField",
+        "numberOfCoalField",
+        "betButton",
+        "halfBetButton",
+        "doubleBetButton",
+      ].forEach((id) => {
+        DOMSelectors[id].disabled = true;
+      });
+
+      DOMSelectors.winField.textContent =
+        "$ " + gameStatus.betAmount.toFixed(2) + " (1.00x)";
+      gameStatus.balance -= gameStatus.betAmount;
+      DOMSelectors.balance.textContent =
+        "Balance: $" + gameStatus.balance.toFixed(2);
+      DOMSelectors.betButton.textContent = "Leave The Factory...";
+      gameStatus.gameInSession = true;
+      gameBoardService.populateCoals();
+      return;
+    }
   },
 };
 
@@ -74,12 +127,23 @@ const handleBetInput = (event) => {
 
 applyConfigs();
 
-DOMSelectors.betAmountField.addEventListener("keydown", handleBetInput);
-DOMSelectors.halfBetButton.addEventListener("click", () =>
-  betSettingService.adjustBet(0.5)
-);
-DOMSelectors.doubleBetButton.addEventListener("click", () =>
-  betSettingService.adjustBet(2)
-);
-DOMSelectors.betAmountField.addEventListener("blur", handleBetInput);
-DOMSelectors.betButton.addEventListener("click", betSettingService.betStart);
+function attachListeners() {
+  DOMSelectors.betAmountField.addEventListener("keydown", handleBetInput);
+  DOMSelectors.halfBetButton.addEventListener("click", () =>
+    betSettingService.adjustBet(0.5)
+  );
+  DOMSelectors.doubleBetButton.addEventListener("click", () =>
+    betSettingService.adjustBet(2)
+  );
+  DOMSelectors.betAmountField.addEventListener("blur", handleBetInput);
+  DOMSelectors.betButton.addEventListener("click", betSettingService.betStart);
+  DOMSelectors.numberOfCoalField.addEventListener("input", (event) =>
+    betSettingService.minesInput(event.target.value)
+  );
+}
+
+function main() {
+  attachListeners();
+}
+
+main();
