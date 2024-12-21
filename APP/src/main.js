de8b3dac16f7c5228516495851adc63b3635e4e8
EmersonYang3 <emersonyang28@gmail.com>
2024-12-21 14:27:18 -0500

    Lose and balance validation after game loss

--- APP/src/main.js
+++ APP/src/main.js
@@ -1,9 +1,11 @@
 const configs = {
   minimumBet: 0.0,
   defaultCoal: 3,
+  houseEdge: 0.02,
   defaultBalance: 1000.0,
-  safePresentImage: "images/coal_pile.png",
+  safePresentImage: "src/images/money.png",
   coalImage: "src/images/coal_pile.png",
+  presentImage: "src/images/present.png",
 };
 
 const DOMSelectors = {
@@ -21,6 +23,8 @@ let gameStatus = {
   gameInSession: false,
   betAmount: configs.minimumBet,
   coalAmount: configs.defaultCoal,
+  winMultiplier: 1.0,
+  winAmount: 0.0,
   balance: configs.defaultBalance,
   safePresentsLeft: 0,
   coalPresentsLeft: 0,
@@ -59,12 +63,33 @@ const gameBoardService = {
 
     for (let i = 0; i < gameStatus.board.length; i++) {
       const button = document.getElementById((i + 1).toString());
+      button.style.backgroundImage = `url(${configs.presentImage})`;
+      button.classList.remove("reveal", "presented", "coal", "safe");
       if (gameStatus.board[i] === 1) {
-        button.style.backgroundImage = `url(${configs.coalImage})`;
+        button.classList.add("coal");
+      } else {
+        button.classList.add("safe");
       }
     }
+  },
 
-    console.log(gameStatus.board);
+  revealPresent(presentID) {
+    return gameStatus.board[presentID - 1] === 1;
+  },
+
+  loseGame() {
+    DOMSelectors.numberOfCoalField.disabled = false;
+    DOMSelectors.betAmountField.disabled = false;
+    DOMSelectors.betButton.textContent = "Open Presents!";
+    gameStatus.gameInSession = false;
+    [
+      "betAmountField",
+      "numberOfCoalField",
+      "halfBetButton",
+      "doubleBetButton",
+    ].forEach((id) => {
+      DOMSelectors[id].disabled = false;
+    });
   },
 };
 
@@ -97,19 +122,22 @@ const betSettingService = {
     if (!gameStatus.betAmount) return;
 
     if (!gameStatus.gameInSession) {
+      if (gameStatus.balance < gameStatus.betAmount) {
+        alert("You don't have enough money to bet that amount!");
+        return;
+      }
+
       [
         "betAmountField",
         "numberOfCoalField",
-        "betButton",
         "halfBetButton",
         "doubleBetButton",
       ].forEach((id) => {
         DOMSelectors[id].disabled = true;
       });
 
-      DOMSelectors.winField.textContent =
-        "$ " + gameStatus.betAmount.toFixed(2) + " (1.00x)";
       gameStatus.balance -= gameStatus.betAmount;
+      gameStatus.safePresentsLeft = 25 - gameStatus.coalAmount;
       DOMSelectors.balance.textContent =
         "Balance: $" + gameStatus.balance.toFixed(2);
       DOMSelectors.betButton.textContent = "Leave The Factory...";
@@ -125,8 +153,6 @@ const handleBetInput = (event) => {
   betSettingService.betInput(event.target.value);
 };
 
-applyConfigs();
-
 function attachListeners() {
   DOMSelectors.betAmountField.addEventListener("keydown", handleBetInput);
   DOMSelectors.halfBetButton.addEventListener("click", () =>
@@ -140,9 +166,38 @@ function attachListeners() {
   DOMSelectors.numberOfCoalField.addEventListener("input", (event) =>
     betSettingService.minesInput(event.target.value)
   );
+
+  Array.from(DOMSelectors.presentContainer.children).forEach((present) => {
+    present.addEventListener("click", (event) => {
+      if (!gameStatus.gameInSession) return;
+
+      present.classList.add("hide");
+      if (present.classList.contains("coal")) {
+        gameStatus.gameInSession = false;
+      }
+
+      setTimeout(() => {
+        present.classList.remove("hide");
+
+        if (present.classList.contains("coal")) {
+          present.style.backgroundImage = `url(${configs.coalImage})`;
+          gameBoardService.loseGame();
+        } else {
+          gameStatus.safePresentsLeft -= 1;
+          present.style.backgroundImage = `url(${configs.safePresentImage})`;
+        }
+        present.classList.add("reveal");
+      }, 400);
+
+      setTimeout(() => {
+        present.classList.add("presented");
+      }, 800);
+    });
+  });
 }
 
 function main() {
+  applyConfigs();
   attachListeners();
 }
 
